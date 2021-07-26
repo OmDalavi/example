ce1b921295e4c2d2e655e56ef1b809a143f42860
Om Dalavi <omdalvi184@gmail.com>
2021-07-26 19:41:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var count=0;
+
+$(".dropdown").on("click",function(){
+	if(count>0){
+		 $(".container3").slideToggle(500);
+		 
+	}
+});
+$(".dropdown").on("click",function(){
+	if(count==0){
+		$(".container3").removeClass("invisible").hide();
+	    $(".container3").slideToggle(500);
+	   
+	    count++;
+	   
+	}
+	// $(".container3").toggle(1000);
+});
+
+
